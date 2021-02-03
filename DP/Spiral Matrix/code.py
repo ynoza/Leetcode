a4f64f71d7a4aa2a60f30c1724c13bea17efdd16
ynoza <46010696+ynoza@users.noreply.github.com>
2021-02-03 16:39:07 -0500

    21/02/03 #1,2

---
+++ DP/Spiral Matrix/code.py
@@ -0,0 +1,46 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        l=0
+        u=0
+        m=len(matrix)
+        n=len(matrix[0])
+        if m==0:
+            return []
+        elif n==1:
+            ret=[]
+            for i in range(len(matrix)):
+                ret.append(matrix[i][0])
+            return ret
+        d=m-1
+        r=n-1
+        end=m*n
+        i,j=0,0
+        ret=[matrix[i][j]]
+        count=1
+        while(j+1<=r):
+            # print("hi")
+            while(j+1<=r):
+                j+=1
+                ret.append(matrix[i][j])
+                count+=1
+            if count==m*n: break
+            u+=1
+            while(i+1<=d):
+                i+=1
+                ret.append(matrix[i][j])
+                count+=1
+            if count==m*n: break
+            r-=1
+            while(j-1>=l):
+                j-=1
+                ret.append(matrix[i][j])
+                count+=1
+            if count==m*n: break
+            d-=1
+            while(i-1>=u):
+                i-=1
+                ret.append(matrix[i][j])
+                count+=1
+            if count==m*n: break
+            l+=1
+        return ret
