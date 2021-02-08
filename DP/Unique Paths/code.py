97869e034d9bb7a1f5e6d3516333f8755fcd1bb7
ynoza <46010696+ynoza@users.noreply.github.com>
2021-02-07 23:52:13 -0500

    21/02/07 #6

---
+++ DP/Unique Paths/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i==0: dp[i][j]=1
+                elif j==0: dp[i][j]=1
+                else: dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[-1][-1]
