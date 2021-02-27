7aff406d94b6f148786376873475989687c209d6
ynoza <46010696+ynoza@users.noreply.github.com>
2021-02-27 14:58:55 -0500

    21/02/27 #2

---
+++ Mathy/Count Primes/iter.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/count-primes/
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n==0: return 0
+        elif n==1: return 0
+        ret=[True] * n
+        
+        for i in range(2,math.ceil(math.sqrt(n))):
+            if ret[i]:
+                for j in range(i*i,n,i):
+                    ret[j]=False
+        # print(ret)
+        return sum(ret)-2
