f6eb5fc92c8419a96b30a551e1fdc1585739b553
ynoza <46010696+ynoza@users.noreply.github.com>
2021-03-14 15:37:42 -0400

    21/03/14

---
+++ Linked List/Swapping Nodes in a Linked List/twoPass2Pointer.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        if head==None: return None
+        elif head.next==None: return head
+        ret=head
+        
+        count=1
+        while(count!=k and count+1!=k):
+            count+=2
+            head=head.next.next
+        if count!=k: 
+            count+=1
+            head=head.next
+            
+        temp_start=count
+        start=head
+        beg=ret
+        
+        while(head and head.next):
+            if head.next.next:
+                head=head.next.next
+                beg=beg.next.next
+            else:
+                head=head.next
+                beg=beg.next
+        
+        # print(start.val,head,beg)
+        
+        temp1=start.val
+        start.val=beg.val
+        beg.val=temp1
+        
+        return ret
