ffe1c9428ca60b734167022ac1aeca14941220e4
ynoza <46010696+ynoza@users.noreply.github.com>
2021-03-25 16:38:12 -0400

    21/03/24 #7

---
+++ Array/Design Circular Queue/iter.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/design-circular-queue/
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.lst=[]
+        self.k=k
+        
+    def enQueue(self, value: int) -> bool:
+        if self.isFull(): return False
+        self.lst.append(value)
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty(): return False
+        self.lst.pop(0)
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty(): return -1
+        return self.lst[0]
+
+    def Rear(self) -> int:
+        if self.isEmpty(): return -1
+        return self.lst[-1]
+
+    def isEmpty(self) -> bool:
+        return len(self.lst)==0
+
+    def isFull(self) -> bool:
+        return len(self.lst)==self.k
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
