2e56677c6938200a98c1f207e4597e89a08981b8
ynoza <46010696+ynoza@users.noreply.github.com>
2021-04-25 18:32:27 -0400

    21/04/25

--- Array/Rotate Image/code.py
+++ Array/Rotate Image/code.py
@@ -7,7 +7,6 @@ class Solution:
         for h in range(math.ceil(height/2)):
             for i in range(h,height-h-1):
                 # print((h,i), (height-i-1,h))
-                # print((height-h-1,height-i-1), (i,height-h-1))
                 temp=matrix[h][i]
                 matrix[h][i] = matrix[height-i-1][h]
                 matrix[height-i-1][h] = matrix[height-h-1][height-i-1]
